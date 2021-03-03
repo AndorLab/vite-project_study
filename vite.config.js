2b2dfbafaf5eb8540d503f8222258503cac40dc3
guzhongren <guzhongren@live.cn>
2021-03-03 22:04:41 +0800

    feat(output): single js file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: "build.js",
+				manualChunks: false,
+			},
+		},
+	},
 	plugins: [reactRefresh()],
 	css: {
 		preprocessorOptions: {
